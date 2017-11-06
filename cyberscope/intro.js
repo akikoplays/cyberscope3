8c3a5e29d6c8c3ddb226fd18b4d1414cf9260420
BorisP <borisp@hiroshima.local>
2017-11-06 13:11:11 +0100

    cyberscope3: - first article implemented - page breaker works - mouse input to browse pages

--- cyberscope/intro.js
+++ cyberscope/intro.js
@@ -10,11 +10,12 @@ var IntroState = function (game){
 };
 
 const CINEMASCOPE = 70;
-const TITLE_Y = 100;
+const TITLE_Y = 90;
 const TEXT_Y = 130;
 const TEXT_X = 20;
 const TEXT_BLOCK_SPACE = 20;
 const TEXT_BLOCK_WIDTH = 350;
+const TEXT_BLOCK_MAX_HEIGHT = 400;
 const LEFT_BLOCK_X = TEXT_X;
 const RIGHT_BLOCK_X = TEXT_X + TEXT_BLOCK_WIDTH + TEXT_BLOCK_SPACE;
 
@@ -67,12 +68,12 @@ IntroState.prototype = {
             game.width, game.height);
         black.endFill();
         that.objs["black"] = black;
-        that.objs["tween"] = game.add.tween(black).to({alpha:0},100,Phaser.Easing.None,true);
+        that.objs["tween"] = game.add.tween(black).to({alpha:0},1000,Phaser.Easing.None,true);
         that.objs["tween"].onComplete.add(function(){
             // another way: 
             // game.time.events.add(2000, onCompleteFn, 0, that);
-            game.time.events.add(100, function() {
-                that.objs["tween"] = game.add.tween(that.objs['midlight']).to({alpha:0.33},500,Phaser.Easing.None,true);
+            game.time.events.add(2000, function() {
+                that.objs["tween"] = game.add.tween(that.objs['midlight']).to({alpha:0.33},1000,Phaser.Easing.None,true);
                 that.showTOC();
             });
         });
@@ -93,7 +94,7 @@ IntroState.prototype = {
         space = TEXT_BLOCK_SPACE;
         size = 16;
 
-        that.objs['gfx'] = game.add.image(game.width + RIGHT_BLOCK_X, TEXT_Y - 30, gfx);
+        that.objs['gfx'] = game.add.image(game.width + RIGHT_BLOCK_X, TEXT_Y /*- 30*/, gfx);
         that.objs['titleSprite'] = game.add.image(LEFT_BLOCK_X, TITLE_Y, that.objs['titleFont']);
         that.objs['titleFont'].text = title;
         that.objs['titleSprite'].x = -that.objs['titleSprite'].width;
@@ -132,29 +133,31 @@ IntroState.prototype = {
                 }
                 
                 that.bmpBlockLeft.text = text.substring(begin, char);
-
-                if (that.bmpBlockLeft.textHeight < 368+16) {
-                    //console.log("Text: " + that.bmpBlockLeft.width + ", " + that.bmpBlockLeft.textHeight + ": " + text.substring(begin, char));
+                var picoffset = 0;
+                if (blockid & 1)
+                    picoffset = that.objs['gfx'].height;
+                if (that.bmpBlockLeft.textHeight < /*368+16*2*/ TEXT_BLOCK_MAX_HEIGHT - picoffset) {
                     lastwordbegin = char;
-
                     if (eot)
                         that.objs['blocks'][blockid++] = text.substring(begin, totalCharsProcessed);
-
                 } else {
                     that.bmpBlockLeft.text = text.substring(begin, lastwordbegin);
                     that.objs['blocks'][blockid++] = text.substring(begin, lastwordbegin);
-
                     begin = lastwordbegin;
                 }
 
-                if (eot)
+                if (eot) {
+                    that.objs['pagesnum'] = Math.round(blockid/2);
+                    that.objs['curpage'] = 0;
+                    that.objs['numblocks'] = blockid;
                     break;
+                }
             }
         }
 
         that.bmpBlockLeft.text = that.objs['blocks'][0];
 
-        that.bmpBlockRight = game.add.bitmapText(game.width + RIGHT_BLOCK_X, y + that.objs['gfx'].height - 26, 'gem', "loading", size);
+        that.bmpBlockRight = game.add.bitmapText(game.width + RIGHT_BLOCK_X, y + that.objs['gfx'].height + 3, 'gem', "loading", size);
         that.bmpBlockRight.tint = 0xeeddff;
         that.bmpBlockRight.maxWidth = width;
         that.bmpBlockRight.text = that.objs['blocks'][1];//text2;
@@ -180,7 +183,11 @@ IntroState.prototype = {
         that.objs['sFooter'] = game.add.sprite(0, game.height+that.objs['footer'].height, that.objs['footer']);
         game.add.tween(that.objs['sFooter']).to({y:game.height - that.objs['footer'].height},1000,Phaser.Easing.Cubic.Out,true);
         game.add.tween(that.objs['sHeader']).to({y:0},1000,Phaser.Easing.Cubic.Out,true);
+
+        // assign listeners for mouse interactions
+        game.input.onDown.add(this.onClick, this);
     },
+
 
     create: function() {
 
@@ -202,4 +209,42 @@ IntroState.prototype = {
 
     },
 
+    onClick: function() {
+        var that = this;
+        if (game.input.mousePointer.isDown) {
+
+            var curpage = that.objs['curpage'];
+            var pagesnum = that.objs['pagesnum'];
+            var numblocks = that.objs['numblocks'];
+            if (game.input.x > that.bmpBlockRight.x 
+                && game.input.y > TEXT_Y && game.input.y < TEXT_Y + TEXT_BLOCK_MAX_HEIGHT) {
+                    console.log("Pressed NEXT");
+                if (curpage < pagesnum) {
+                    curpage++;
+                    var lp = curpage*2;
+                    var rp = lp+1;
+                    that.bmpBlockLeft.text = that.objs['blocks'][lp];
+                    if (rp < numblocks)
+                        that.bmpBlockRight.text = that.objs['blocks'][rp];
+                    else
+                        that.bmpBlockRight.text = "";
+                    that.objs['curpage'] = curpage;
+                }
+            } else if (game.input.x > that.bmpBlockLeft.x && game.input.x < that.bmpBlockLeft.x + that.bmpBlockLeft.width 
+                && game.input.y > TEXT_Y && game.input.y < TEXT_Y + TEXT_BLOCK_MAX_HEIGHT) {
+                console.log("Pressed PREV");
+                if (curpage > 0) {
+                    curpage--;
+                    var lp = curpage*2;
+                    var rp = lp+1;
+                    that.bmpBlockLeft.text = that.objs['blocks'][lp];
+                    if (rp < numblocks)
+                        that.bmpBlockRight.text = that.objs['blocks'][rp];
+                    else
+                        that.bmpBlockRight.text = "";
+                    that.objs['curpage'] = curpage;
+                }
+            }
+        }
+    },
 };
